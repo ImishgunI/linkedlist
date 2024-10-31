0e4928679dc90810a12e33c28f9d9f35c6dfdf69
Michael <mvasilyev04@gmail.com>
2024-10-31 20:19:29 +0300

    write a 3 func for test

--- module_tests/test.c
+++ module_tests/test.c
@@ -16,6 +16,66 @@ void push_test() {
     }
 }
 
+void pop_test() {
+    list* node = NULL;
+    push(&node, 10);
+    push(&node, 20);
+    push(&node, 30);
+
+    printf("Before pop: %d\n", node->value);
+    pop(&node);
+    printf("After pop: %d\n", node->value);
+}
+
+void is_empty_test() {
+    list* node = NULL;
+    push(&node, 10);
+    if(is_empty(node) == 0) {
+        printf("SUCCESS");
+    } else {
+        printf("FAIL");
+    }
+}
+
+void size_test() {
+    list* node = NULL;
+    push(&node, 10);
+    push(&node, 20);
+    push(&node, 30);
+
+    int len = size(node);
+    if(len > 0) {
+        printf("len: %d\n", len);
+        printf("SUCCESS");
+    } else {
+        printf("FAIL");
+    }
+}
+
+void find_test() {
+    list* node = NULL;
+    push(&node, 10);
+    push(&node, 20);
+    push(&node, 30);
+
+    list* elem = find(node, 10);
+    if(elem != NULL) {
+        printf("elem is %d\n", elem->value);
+        printf("SUCCESS");
+    } else {
+        printf("FAIL");
+    }
+}
+
 int main() {
+    printf("TEST PUSH\n");
     push_test();
+    printf("\n\nTEST POP\n");
+    pop_test();
+    printf("\n\nTEST IS_EMPTY\n");
+    is_empty_test();
+    printf("\n\nTEST SIZE\n");
+    size_test();
+    printf("\n\nTEST FIND\n");
+    find_test();
 }
